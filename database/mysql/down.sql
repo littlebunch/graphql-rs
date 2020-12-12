f168f34242332972bd47c24cabd79f8af3d463cb
Gary Moore <littlebunch@gmail.com>
2020-12-12 08:51:36 -0500

    rename pg branch to master

--- database/mysql/down.sql
+++
@@ -1,7 +0,0 @@
--- This file should undo anything in `up.sql`
-drop table nutrient_data;
-drop table derivations;
-drop table nutrients;
-drop table foods;
-drop table food_groups;
-drop table manufacturers;
